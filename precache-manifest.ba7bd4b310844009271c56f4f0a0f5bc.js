080672f66d5b8d14b328eace1d08e70f0eaa40f1
Sergio Rios <sergioriosp04@gmail.com>
2019-09-10 11:11:59 -0500

    Updates

---
+++ precache-manifest.ba7bd4b310844009271c56f4f0a0f5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cba62e3c465c22ff27038244de01cf0",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "b9d24b2cbba787778829",
+    "url": "/Notes/static/css/2.cb0a7b97.chunk.css"
+  },
+  {
+    "revision": "e818cf7bb61575d77520",
+    "url": "/Notes/static/css/main.39129754.chunk.css"
+  },
+  {
+    "revision": "b9d24b2cbba787778829",
+    "url": "/Notes/static/js/2.2fb16ccb.chunk.js"
+  },
+  {
+    "revision": "e818cf7bb61575d77520",
+    "url": "/Notes/static/js/main.3e86648a.chunk.js"
+  },
+  {
+    "revision": "391e80cb8c5f9ec048c9",
+    "url": "/Notes/static/js/runtime~main.82dbbc2d.js"
+  }
+]);
